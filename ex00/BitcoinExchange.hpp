b00f22822f9a1f83e347a34cf640c4f1eb2fb357
Aziz Fadouach <afadouac@e1r12p8.1337.ma>
2025-05-11 22:50:42 +0100

    first exercice

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,80 @@
+#pragma once 
+
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <bits/stdc++.h>
+#include <cstring> 
+
+class BitcoinExchange
+{
+    private:
+        std::map<std::string, float> dataBase;
+    
+    public:
+        BitcoinExchange(/* args */);
+        ~BitcoinExchange();
+        void    fillDataBase();
+        void    findElement(std::string input, float rate);
+};
+
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+void    BitcoinExchange::findElement(std::string input , float rate){
+
+    std::map<std::string, float>::iterator it = dataBase.lower_bound(input);
+
+    if (it != dataBase.end() && it->first == input) {
+        std::cout << input << " => "<< rate << " = " << rate * it->second << std::endl;
+
+        // std::cout << it->second << std::endl;
+    } else {
+        if (it == dataBase.begin()) {
+            
+            throw std::runtime_error("No earlier date available in the database.");
+        } else {
+            --it;
+            std::cout << input << " => "<< rate << "= " << rate * it->second << std::endl;
+            // std::cout << it->second << std::endl;
+        }
+    }
+}
+
+
+void BitcoinExchange::fillDataBase()
+{
+    std::ifstream dataFile("data.csv");
+
+    if (!dataFile.is_open())
+        throw std::runtime_error("Failed to open data File.");
+
+    std::string line;
+
+    std::getline(dataFile, line);
+    if (line != "date,exchange_rate")
+        throw std::runtime_error("syntax error in csv.");
+
+    while (std::getline(dataFile, line)) {
+        size_t commaPos = line.find(",");
+
+        std::string date = line.substr(0, commaPos);
+        std::string rateStr = line.substr(commaPos + 1);
+
+        dataBase[date] = std::atof(rateStr.c_str());
+    }
+    ////////////////////////////////////
+    // for(auto ele: dataBase)
+    // {
+    //     std::cout << ele.first << " " << ele.second << std::endl;
+    // }
+    ///////////////////////////////////
+    dataFile.close();
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,100 @@
+#include "BitcoinExchange.hpp"
+
+
+std::vector<std::string> splitBySpace(const std::string& input) {
+    std::vector<std::string> tokens;
+    std::istringstream iss(input);
+    std::string token;
+
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+// int main() {
+//     std::string line = "this is a test string";
+//     std::vector<std::string> words = splitBySpace(line);
+
+//     for (size_t i = 0; i < words.size(); ++i)
+//         std::cout << words[i] << std::endl;
+
+//     return 0;
+// }
+
+bool isValidDate(std::string date)
+{
+    if (date.size() != 10)
+        return false;
+    if (date[4]  != '-' || date[7] != '-')
+        return false;
+    for (size_t i = 0; i < date.size(); i++)
+    {
+        if (i == 4 || i == 7) continue ;
+
+        if (!isdigit(date[i]))
+            return false;
+    }
+    return true;
+}
+
+int main(int ac, char **av){
+
+    if (ac != 2)
+    {
+        std::cout << "invalid  arguiments count " << std::endl;
+        return 1;
+    }
+    BitcoinExchange dataBase;
+    dataBase.fillDataBase();
+    std::ifstream inputFile(av[1]);
+
+
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Failed to open File." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    std::getline(inputFile, line);
+    if ("date | value" != line){
+        std::cerr << "invalid start line of the file !!!" << std::endl;
+    }
+
+    while (std::getline(inputFile, line)) {
+        std::vector<std::string> splited = splitBySpace(line);
+
+        if (splited.size() != 3){
+            std::cout << "Error: bad input  => " << line << std::endl;
+            continue ;
+        }
+        if (splited[1] != "|"){
+            std::cerr << "syntax error !!!" << std::endl;
+            continue ;
+        }
+        if (!isValidDate(splited[0]))
+        {
+            std::cerr << "Invalid date format !!!" << std::endl;
+            continue ;
+        }
+        float rate = std::atof(splited[2].c_str());
+
+        if (rate < 0){
+            std::cerr << "Error: not a positive number." << std::endl;
+            continue ;
+        }
+
+        if (rate > 1000){
+            std::cerr << "Error: too large a number." << std::endl;
+            continue ;
+        }
+
+        dataBase.findElement(splited[0], rate);
+    }
+
+    inputFile.close();
+
+    return 0;
+
+}
