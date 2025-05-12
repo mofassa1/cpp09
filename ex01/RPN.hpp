4d30b9a375f54b9eaed67eff60c8a0e8e421acd6
Aziz Fadouach <afadouac@e1r12p8.1337.ma>
2025-05-12 19:29:09 +0100

    ex01 added

---
+++ ex01/RPN.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <stack>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <cstring> 
+
+
+class RPN
+{
+    private:
+        std::stack <int> stck;
+        std::string arguiments;
+
+    public:
+        RPN();
+        RPN(std::string args);
+        RPN(const RPN &other);
+        RPN &operator=(const RPN &other);
+        void calculation();
+        ~RPN();
+};
+
+RPN::RPN()
+{
+}
+
+RPN::RPN(std::string args): arguiments(args)
+{
+}
+
+RPN::RPN(const RPN& other) : arguiments(other.arguiments) {
+    stck = other.stck; 
+}
+
+
+RPN& RPN::operator=(const RPN& other) {
+    if (this != &other) {
+        arguiments = other.arguiments;
+        stck = other.stck;
+    }
+    return *this;
+}
+
+
+
+void RPN::calculation(){
+
+    std::istringstream iss(arguiments);
+    std::string token;
+
+    while (iss >> token) {
+
+        if (token.size() == 1 && !isdigit(token[0]) && stck.size() >= 2)
+        {
+            int value = stck.top();
+            stck.pop();
+            if (token == "+")
+                value = value + stck.top();
+            else if (token == "-")
+                value =  stck.top() - value;
+            else if (token == "*")
+                value =  stck.top() * value;
+            else if (token == "/" && value != 0)
+                value =  stck.top() / value;
+            else
+                throw std::runtime_error("invalid opperation !!");
+            
+            stck.pop();
+
+            stck.push(value);
+        }
+        else if (token.size() == 1 && isdigit(token[0]))
+        {
+            int value = atoi(token.c_str());
+
+            stck.push(value);
+        }
+        else
+            throw std::runtime_error("INVALID OPERATION !!");
+    }
+    if (stck.size() != 1)
+        throw std::runtime_error("INVALID operation !!");
+
+    std::cout << stck.top() << std::endl;
+}
+
+
+
+
+RPN::~RPN()
+{
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,23 @@
+#include "RPN.hpp"
+
+
+
 int main(int ac, char **av){
 
+    if (ac != 2){
+        std::cerr << "Invalid arguiment count !!" << std::endl;
+        return 1;
+    }
 
-
+    try
+    {
+        RPN rpn(av[1]);
+        
+        rpn.calculation();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 }
